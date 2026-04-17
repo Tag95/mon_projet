6fef8d6f7b1b7f451c4150fc86ee08bed50b27f4
Tag95 <gaeltoe21@gmail.com>
2026-04-17 18:22:16 +0000

    Add API key (test gitleaks)

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
 def addition(a, b):
     return a + b
+
+# Mauvaise pratique — clé en dur
+API_KEY = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+
+# Mauvaise pratique — clé en dur
+API_KEY = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
