6b9f464b5d0a7f4db5e71576343dbd2c22aca19d
Tag95 <gaeltoe21@gmail.com>
2026-04-18 15:02:20 +0000

    Fix: remove hardcoded secrets

--- src/main.py
+++ src/main.py
@@ -1,7 +1,2 @@
 def addition(a, b):
     return a + b
-
-# Mauvaise pratique — clé en dur
-API_KEY = "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ123456789"
-
-AWS_SECRET = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
